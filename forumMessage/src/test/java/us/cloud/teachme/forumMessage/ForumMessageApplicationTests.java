ef3c158615c3faa66fc5118b4a02a1faeff264a8
marreylop4 <marreylop4@alum.us.es>
2024-11-17 11:18:16 +0100

    Feat: Correct structure

---
+++ forumMessage/src/test/java/us/cloud/teachme/forumMessage/ForumMessageApplicationTests.java
@@ -0,0 +1,13 @@
+package us.cloud.teachme.forumMessage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForumMessageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
